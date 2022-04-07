55aefdf6440cfb1761a0f61f0b900898dff1a7a8
tashteba <91596532+tashteba@users.noreply.github.com>
2022-04-07 08:37:17 +0200

    Add files via upload

---
+++ Formulaire php7/index.php
@@ -0,0 +1,13 @@
+<?php 
+session_start();
+ require ('actions/users/secuirtyAction.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php   include 'includes/head.php' ; ?>
+<body>
+    <?php include 'includes/navbar.php';  ?>
+    <br><br>
+</body>
+</html>
